b78a09adfdaed9cb968f2f9d61ab79af8e9274b0
Gustavo Heiden <gustavo.heiden32@gmail.com>
2026-03-03 21:23:46 -0300

    pesquisaBinaria

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿System.Console.WriteLine("escreva um numero de 1 a 20 , e o programa\nvai adivinhar");
+
+
+
+int min = 0;
+int max = 20;
+int tentativas = 0;
+
+Console.WriteLine("Pressione ENTER quando estiver pronto.");
+Console.ReadLine();
+
+while (true)
+{
+    int meio = (min + max) /2;
+    tentativas++;
+    System.Console.WriteLine($"seu numero é {meio}?");
+    System.Console.WriteLine("digite:\n[ 1 ]maior\n[ 2 ]menor\n[ 3 ]acertou");
+    String resposta = Console.ReadLine();
+
+    if(resposta ==  "1")
+    {
+        min = meio + 1;
+    }
+    else if (resposta == "2")
+    {
+      max = meio-1;  
+    }
+    else if(resposta == "3")
+    {
+        System.Console.WriteLine($"Acertei em {tentativas} tentativas!");
+        break;
+    }
+    else
+    {
+        System.Console.WriteLine("resposta invalida ");
+    }
+    
+}
--- obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
+++ obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8ff63d274191ea51e037847020e0675e9570a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
